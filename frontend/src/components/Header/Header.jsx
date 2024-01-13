2cb14b95a3090c2881771f8d2fce869638cb700b
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-01-13 18:05:08 +0530

    hero_section worked

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import { FaSearch } from "react-icons/fa";
 
 function Header() {
   return (<>
-    <div className="container bg-[#4abdac] ">   
+    <div className="container bg-[#4abdac] border-b-4 ">   
         <div className="main_header flex pt-7">  
-            <div className="header_log flex space-x-2 ml-16">
+            <div className="header_log flex space-x-2 -mt-2 ml-16">
                 <div className="logo">
                 <FaSearch className="logo_icon w-16  h-16 bg-[#f7b733] text-[#f2f0f4]  border-4 rounded-full p-2"/>
                 </div>
@@ -17,19 +17,19 @@ function Header() {
 
             </div>
             {/* middle -- content */}
-            <div className="middle_header flex ml-[280px] space-x-5 text-[20px] text-[#f2f0f4]">
-                <p>Home</p>
-                <p>Browse Job </p>
-                <p>Pages</p>
-                <p>Blog</p>
-                <p>Contact</p>
+            <div className="middle_header flex ml-[280px] space-x-5 text-[20px] font-semibold cursor-pointer text-[#f2f0f4] ">
+                <p className="hover:text-[#f7b733]">Home</p>
+                <p className="hover:text-[#f7b733]">Browse Job </p>
+                <p className="hover:text-[#f7b733]">Pages</p>
+                <p className="hover:text-[#f7b733]">Blog</p>
+                <p className="hover:text-[#f7b733]">Contact</p>
 
             </div>
             {/* end -- content  */}
-            <div className="end_header flex text-[#f2f0f4] space-x-2 ml-[370px] ">
-                <div className="login text-[19px]">Log in</div>
-                <div className="job_post text-[20px] ">
-                    <span className="txt"> Post A Job</span>
+            <div className="end_header flex text-[#ffffff] space-x-2 ml-[330px] cursor-pointer font-semibold">
+                <div className="login text-[19px] hover:text-[#f7b733]">Log in</div>
+                <div className="job_post text-[20px] bg-[#f7b733] hover:bg-[#4abdac] h-[35px] w-[110px] pl-2 rounded-sm">
+                    <span className="txt hover:text-[#f7b733]  text-[white]"> Post A Job</span>
                 </div>
             </div>
         </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 
 import Header from "../components/Header/Header"
+import img from "./home.jpg"
 
 
 function Home() {
@@ -7,7 +8,24 @@ function Home() {
     <>
     <Header/>
 
-    <div className="homr text-6xl"> clasHome</div>
+    <div className="container relative group">
+        <img src={img} alt="" className="object-cover w-full h-full opacity-90" />
+        <div className="hero absolute inset-0 bg-[#4abdac] opacity-60"></div>
+        <div className="hero_section ml-[230px] mt-[210px] absolute inset-0 flex flex-col ">
+          <div className="job_numbers text-[#f2eded] font-medium text-[30px]">
+            <p>4500+ Job Listings.</p>
+          </div>
+          <div className="header_title font-semibold text-[#f2eded] text-[60px]">
+            <span>Find your Dream Job</span>
+          </div>
+          <div className="slogan text-[24px] text-[#f2eded]">
+            <span>we Providing Best job list from all over India</span>
+          </div>
+          <div className="resume_uploder font-medium text-[20px] bg-[#f7b733] w-[290px] h-auto text-center p-2 mt-3 rounded-md">
+            <button className="upload_resume hover:text-[white]">Upload Your Resume</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
